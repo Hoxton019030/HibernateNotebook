d49c7c9762ca992af277dd113815cb051312cbc8
Hoxton <za546321@yahoo.com.tw>
2022-04-25 19:48:44 +0800

    no message

--- src/tw/hibernatedemo/model/CompanyBean.java
+++ src/tw/hibernatedemo/model/CompanyBean.java
@@ -2,8 +2,18 @@ package tw.hibernatedemo.model;
 
 public class CompanyBean {
 
+	private int companyId;
+	
 	public CompanyBean() {
-		// TODO Auto-generated constructor stub
 	}
+	
 
 }
+
+/*SQL語法
+CREATE DATABASE hibernateDB
+USE hbiernateDB
+CREATE TABLE company(
+companyId INT PRIMARY KEY NOT NULL,
+compantName NVARCHAR(50) NOT NULL,)
+ */
